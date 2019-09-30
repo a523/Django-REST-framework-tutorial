6037e212ff3c8de9045c1910a9cd09adc63b55cf
雷永鑫 <yongxin_lei@sui.com>
2019-09-30 14:51:23 +0800

    Tutorial 6: 视图集和路由器

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,18 +1,17 @@
-from django.urls import path
 from django.conf.urls import include
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
 from snippets import views
 
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'snippets', views.SnippetViewSet)
+
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>', views.UserDetail.as_view(), name='user-detail'),
-    path('', views.api_root),
+    path('', include(router.urls))
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
 urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
 ]
